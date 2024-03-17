c06ef6b7bcf5020eb4a8d620bc0c7275a75382df
amit <amitkumar.singh@vsit.edu.in>
2024-03-17 12:20:26 +0000

    cleared WKS

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name='login'),
-    # path('<slug>/' , views.get_product , name="get_product"),
-]
-
-
--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   
-    path('product/<slug>/' , views.get_product , name="get_product"),
-]
